14881e7934fc955f5c719c82df7da1af7932eaea
ansudiscord <98954057+ansudiscord@users.noreply.github.com>
2022-04-27 14:29:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function image1()
+{
+    window.location="bedroom.html";
+}
+
+function image2()
+{
+    window.location="living_room.html";
+}
+
+function image3()
+{
+    window.location="bottle.html";
+}
